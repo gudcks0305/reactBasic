db98b2e98077dc2cee6163b8df70911682b967c7
unknown <gudcks305@gmail.com>
2021-08-12 16:13:42 +0900

    로그인 기능

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const {user, User} = require('./models/User')
 const bodyParser = require('body-parser')
 const config = require('./config/key')
+const cookieParser = require('cookie-parser')
 app.use(bodyParser.urlencoded({extended:true}))
 
 app.use(bodyParser.json());
@@ -16,7 +17,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!rr')
 })
 app.post('/register',(req,res)=> {
-  // 회원가입 필요정보 
+  // 회원가입 필요정보
   const user = new User(req.body)
   user.save((err,userInfo)=>{
     if(err) return res.json({success : false, err})
@@ -25,4 +26,34 @@ app.post('/register',(req,res)=> {
     })
   })
 })
-app.listen(3000,console.log('is goo'))
+app.post('/login',(req,res)=>{
+  // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({email : req.body.email}, (err,user)=>{
+    if(!user){ 
+      return res.json({
+        loginSuccess: false,
+        message : "제공된 이메일과 맞는 유저가 없습니다."
+      })
+    }
+    // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀 번호인지 확인
+  user.comparePassword(req.body.password,(err,isMatch)=>{
+    console.log(err,isMatch)
+    if(!isMatch){
+      return res.json({loginSuccess : false, message : "로그인이 실패했습니다."})
+    }
+
+
+    
+  })
+  // 비밀번호까지 맞다면 토큰 생성 
+  user.generateToken((err,user)=>{
+    if(err) return res.status(400).send(err)
+    //토큰을 어디에 저장?? 로컬 , 쿠키 ??
+    res.cookie('x_auth',user.token).status(200).json({loginSuccess:true,userId:user._id})
+  })
+  })
+  
+})
+  
+ 
+app.listen(3000,console.log('is good'))
